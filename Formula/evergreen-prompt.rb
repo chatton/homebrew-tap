ee2b8e32639c4766cffdae2ba11fcc598af04881
chatton <cianhatton@gmail.com>
2021-04-05 20:54:46 +0100

    updated to version 1.0.1

--- Formula/evergreen-prompt.rb
+++ Formula/evergreen-prompt.rb
@@ -6,8 +6,8 @@ class EvergreenPrompt < Formula
   homepage "https://github.com/chatton/evergreen-prompt"
   version "1.0.0"
   license "MIT"
-  url "https://github.com/chatton/evergreen-prompt/releases/download/v1.0.0/evergreen-prompt-1.0.0.tar.gz"
-  sha256 "07dd20b1534844953413100c91b023e0bec6be0fb2162c847bf6104aaac053f1"
+  url "https://github.com/chatton/evergreen-prompt/releases/download/v1.0.0/evergreen-prompt-1.0.1.tar.gz"
+  sha256 "e90ee3d40d57dc4c8937ce89d53f7195be50a3e1c01a29a7a0b8085a3e94ba53"
   bottle :unneeded
 
 

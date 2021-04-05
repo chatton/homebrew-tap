64280a820240d82ec233df1a364db02fdccec56f
chatton <cianhatton@gmail.com>
2021-04-05 21:00:03 +0100

    update version

--- Formula/evergreen-prompt.rb
+++ Formula/evergreen-prompt.rb
@@ -4,7 +4,7 @@
 class EvergreenPrompt < Formula
   desc "Interactive CLI for Evergreen"
   homepage "https://github.com/chatton/evergreen-prompt"
-  version "1.0.0"
+  version "1.0.1"
   license "MIT"
   url "https://github.com/chatton/evergreen-prompt/releases/download/v1.0.1/evergreen-prompt-1.0.1.tar.gz"
   sha256 "e90ee3d40d57dc4c8937ce89d53f7195be50a3e1c01a29a7a0b8085a3e94ba53"
